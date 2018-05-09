81f53720ebdee53cc65c2577521fcd01c8060992
havanduy <havanduy709@gmail.com>
2018-05-10 06:05:32 +0700

    ok

--- server/database/database.js
+++ server/database/database.js
@@ -1,2 +1,3 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/quanlynhansu");
+mongoose.connect("mongodb://havanduy:havanduy@ds217360.mlab.com:17360/quanlynhansu");
+// mongoose.connect("mongodb://localhost:27017/quanlynhansu");
